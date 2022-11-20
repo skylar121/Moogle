3c8ce859d16dae102b3a56a57234960daa0b53c6
skylar121 <myway01212@naver.com>
2022-11-20 17:51:41 +0900

    chore: 장고 어드민 등록

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Movie, Genre, Community, Comment, Review, ReviewComment
 
 # Register your models here.
+
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(Community)
+admin.site.register(Comment)
+admin.site.register(Review)
+admin.site.register(ReviewComment)
