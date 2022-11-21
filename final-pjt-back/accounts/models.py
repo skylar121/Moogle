17511fd07eaff3434ab1fa337318c48d49258146
류태규 <estar1996@naver.com>
2022-11-21 17:24:09 +0900

    feat: 프로필, Ganre.name 접근, User 수정

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    follow = models.ManyToManyField('self',symmetrical=False,related_name='followers')
     # users = models.CharField(max_length=50)
     # password = models.CharField(max_length=50)
     # nickname = models.CharField(max_length=50)
--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,8 +1,64 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
 
 
 # def some_view_func(request):
 #     token = Token.objects.create(user=...)
 #     return Response({'token':token.key})
+
+
+
+
+
+
+# def profile(request, username):
+#     User =get_object_or_404(get_user_model(),username=username)
+
+#     person = User.objects.get(username=username)
+
+#     return Response({'username': username, })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def follow(request, user_pk):
+#     if request.user.is_authenticated:
+#         User = get_user_model()
+#         person = User.objects.get(pk=user_pk)
+#         if person != request.user:
+#             if person.followers.filter(pk=request.user.pk).exists():
+#                 person.followers.remove(request.user)
+#             else:
+#                 person.followers.add(request.user)
+#         return redirect('accounts:profile',person.username)
+#     return redirect('accounts:login')
+
+
